576cb11cba51febb70f03732d288e020732059dc
RISAN-96 <risanhere@gmail.com>
2026-07-17 16:17:28 +0600

    July 17 - T36

--- July17/T36.java
+++
@@ -1,7 +0,0 @@
-package July17;
-
-public class T36 {
-    public static void main(String[] args) {
-
-    }
-}
---
+++ July17/T36_OOP.java
@@ -0,0 +1,21 @@
+package July17;
+// Chapter 8: Intro to OOP
+// Object oriented programing tries to map code instructions with real world making the code short and easier to understand
+// What is OOP: Solving a problem by creating objects is called OOP
+// What is DRY: DRY stands for Do not repeat yourself - focuses on code reusability
+// Class: A class is a blueprint for creating Objects.
+// [Class] => Object Instantiation => [Object]
+// Class contains info to create a valid object
+// Object: Object is a instantiation of a class. When a class is defined, a template (info) is defined. Memory is allocated only after object instantiation.
+// How to model a problem in OOPs
+// We identify the following:
+// Noun -> Class -> Employee
+// Adjective -> Attribute -> NAme, Age, Salary
+// Verb -> Methods -> getSalary(), increment()
+
+
+public class T36_OOP {
+    public static void main(String[] args) {
+        System.out.println("Object Oriented Programing");
+    }
+}
