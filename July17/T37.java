0e6a850366520d435ae473aec153167b0889863a
RISAN-96 <risanhere@gmail.com>
2026-07-17 16:45:01 +0600

    July 17 - T37

--- July17/T37.java
+++
@@ -1,7 +0,0 @@
-package July17;
-
-public class T37 {
-    public static void main(String[] args) {
-
-    }
-}
---
+++ July17/T37_Terminologies.java
@@ -0,0 +1,29 @@
+package July17;
+//Java Tutorial: Basic Terminologies in Object Oriented Programming
+// OOPs terminology
+
+// Abstraction -> Hiding internal details (show only essential info!)
+// Example: We use phone without bothering about how it was made.
+
+// Encapsulation -> The act of putting various components together (in a capsule)
+// Example: laptop is a single entity wit hwifi + speaker + storage in a single box!
+
+// In java capsulation simply means that the sensitive data can be hidden from the users
+
+// Inheritance: The act of deriving new things from existing things.
+// Rickshaw => E-Rickshaw
+// Phone => SmartPhone                   -- implements DRY!
+
+// Polymorphism: One entity many forms
+// SmartPhone -> Phone
+// SmartPhone -> Calculator
+// SmartPhone -> Camera
+// SmartPhone -> Music player
+
+public class T37_Terminologies {
+    public static void main(String[] args) {
+        System.out.println("Basic Terminologies in Object Oriented Programming");
+        System.out.println("Java Tutorial: Basic Terminologies in Object Oriented Programming");
+        System.out.println("OOPs terminology");
+    }
+}
