8f86339142a79b3a5dc880153ab5503ab14e0be0
RISAN-96 <risanhere@gmail.com>
2026-09-14 09:15:20 +0600

    September 14

--- September14/T65_Creating_Packages_in_java.java
+++ September14/T65_Creating_Packages_in_java.java
@@ -1,4 +1,15 @@
 package September14;
 
+//import java.util.Scanner;
+//import java.util.*; // * dile sobkicu import hoea jabe alada alada kore import korte hobena
 public class T65_Creating_Packages_in_java {
+    public static void main(String[] args) {
+//        Scanner sc = new Scanner(System.in);
+        java.util.Scanner sc = new java.util.Scanner(System.in); // directly hardcoaded
+        int n = sc.nextInt();
+        System.out.println(n);
+
+    }
 }
+//package onujayee compile korar jonno command: javac -d . *.java
+//aita korle sob java file tader corrosponding package er vetore compile hobe
